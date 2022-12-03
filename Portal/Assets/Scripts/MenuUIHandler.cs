8780b0093e3c99ebc8e8e809616292207332037a
Gal Bracha <galbra@gmail.com>
2022-12-03 20:48:34 +0200

    Added initial menus

---
+++ Portal/Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
