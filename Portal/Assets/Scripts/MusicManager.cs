f8a82f454327b024a521826f50e0a0917abc32ef
Gal Bracha <galbra@gmail.com>
2022-12-11 22:35:31 +0200

    Added shuffle music player

--- Portal/Assets/Scripts/MusicManager.cs
+++ Portal/Assets/Scripts/MusicManager.cs
@@ -1,46 +1,103 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
+using Random = UnityEngine.Random;
 
+/**
+ * Shuffles music folders
+ * Pick Next folder
+ * Shuffle the files
+ * Play them one after the other
+ * And Repeat the process
+ */
 public class MusicManager : MonoBehaviour
 {
     [SerializeField] private List<string> mp3Directories;
-    private List<AudioClip> currentPlaylist = new List<AudioClip>();
+    private List<AudioClip> currentPlaylist = new();
     private string musicRootPath = "music";
+    private AudioSource source;
+    private string chosenFolder;
+    private FileInfo[] chosenFolderFiles;
+    private List<int> filesPlayOrder;
+    private List<int> foldersPlayOrder = new();
+    
+    const int NUMBER_OF_FOLDER_REPEATS = 2;
     
     void Start()
     {
+        source = GetComponent<AudioSource>();
         Debug.Log("Loading music from " + Application.dataPath);
+        InitMusic();
+    }
+
+    void InitMusic()
+    {
+        Debug.Log("Init Music");
+        if (mp3Directories.Count <= 0)
+        {
+            Debug.LogError("Please set mp3 directories names for sound to play");
+            return;
+        }
+        ShuffleFolders();
+        PlayNextFolder();
+    }
+
+    void PlayNextFolder()
+    {
+        PickNextFolder();
+        ShuffleSongsInFolder();
         PlayRandomSong();
+    }
+
+    private void ShuffleFolders()
+    {
+        for (var i=0; i < NUMBER_OF_FOLDER_REPEATS; i++)
+        {
+            var folderShuffle = Enumerable.Range(0, mp3Directories.Count - 1).ToList();
+            KnuthShuffleArray(folderShuffle);
+            foldersPlayOrder.AddRange(folderShuffle);
+        }
+    }
+    
+    private void PickNextFolder()
+    {
+        var isFinishedLoopingAllFolder = (foldersPlayOrder.Count <= 0); 
+        if (isFinishedLoopingAllFolder)
+        {
+            InitMusic();
+            return;
+        }
+        var folderIndexToPlay = foldersPlayOrder[0];
+        foldersPlayOrder.RemoveAt(0);
+        chosenFolder = Path.Combine(Application.dataPath, musicRootPath, mp3Directories[folderIndexToPlay]);
+        Debug.Log("Chosen " + chosenFolder + " music");
+    }
+
+    private void ShuffleSongsInFolder()
+    {
+        Debug.Log("Playing next random");
+        chosenFolderFiles = GetAllMp3InDirectory(chosenFolder);
+        filesPlayOrder = Enumerable.Range(0, chosenFolderFiles.Length - 1).ToList();
+        KnuthShuffleArray(filesPlayOrder);
     }
 
     void PlayRandomSong()
     {
-        var fullPath = GetRandomDirectoryPath();
-        if (fullPath == "") return;
-        var files = GetAllMp3InDirectory(fullPath);
-        var randomFileIndex = Random.Range(1, files.Length - 1);
-        StartCoroutine(GetAudioClip(files[randomFileIndex].FullName, currentPlaylist));
-    }
-
-    string GetRandomDirectoryPath()
-    {
-        int directoryToUse = PickRandomDirectory();
-        if (directoryToUse != -1) return "";
-        string directoryPath = mp3Directories[directoryToUse];
-        return Path.Combine(Application.dataPath, musicRootPath, directoryPath);
-    }
-
-    int PickRandomDirectory()
-    {
-        if (mp3Directories.Count <= 0)
+        var hasFinishedPlayingAllFilesInFolder = (filesPlayOrder.Count <= 0); 
+        if (hasFinishedPlayingAllFilesInFolder)
         {
-            Debug.LogError("Please set mp3 directories names for sound to play");
-            return -1;
+            PlayNextFolder();
+            return;
         }
-        return Random.Range(1, mp3Directories.Count - 1);
+        
+        var fileIndexToPlay = filesPlayOrder[0];
+        filesPlayOrder.RemoveAt(0);
+        var fileToPlay = chosenFolderFiles[fileIndexToPlay];
+        StartCoroutine(PlayAudio(fileToPlay.FullName, fileToPlay.Name,  currentPlaylist));
     }
 
     FileInfo[] GetAllMp3InDirectory(string path)
@@ -49,19 +106,49 @@ public class MusicManager : MonoBehaviour
         return dir.GetFiles("*.mp3");
     }
 
-    IEnumerator GetAudioClip(string fullFilePath, List<AudioClip> audioClips)
+    IEnumerator PlayAudio(string fullFilePath, string fileName, List<AudioClip> audioClips)
     {
-        UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip(fullFilePath, AudioType.MPEG);
-        yield return webRequest.SendWebRequest();
-        if(webRequest.isNetworkError)
+        if (!File.Exists(fullFilePath))
         {
+            Debug.LogError("File doesnt exist: " + fullFilePath);
+            yield break;
+        }
+        Debug.Log("Loading " + fullFilePath);
+        UnityWebRequest webRequest = UnityWebRequestMultimedia.GetAudioClip("file://" + fullFilePath, AudioType.MPEG);
+        yield return webRequest.SendWebRequest();
+        if(webRequest.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.LogError("Cant play music");
             Debug.LogError(webRequest.error);
         }
         else
         {
             AudioClip clip = DownloadHandlerAudioClip.GetContent(webRequest);
-            clip.name = fullFilePath;
+            clip.name = fileName;
             audioClips.Add(clip);
+            PlayNext(clip);
         }
     }
+
+    private void PlayNext(AudioClip clip)
+    {
+        Debug.Log("Playing Music" + clip.name);
+        source.clip = clip;
+        source.Play();
+        // Play another random - upon song finished playing
+        Debug.Log("Playing another song in " + clip.length + "Time");
+        Invoke(nameof(PlayRandomSong), clip.length);
+    }
+    
+    void KnuthShuffleArray(List<int> array)
+    {
+        for (int t = 0; t < array.Count; t++ )
+        {
+            var tmp = array[t];
+            var r = Random.Range(t, array.Count);
+            array[t] = array[r];
+            array[r] = tmp;
+        }
+    }
+
 }
